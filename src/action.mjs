7dbabf88381e5930b297c6811df7c3da0a9f8fa5
plushveil <plushveil@gray.blue>
2024-09-27 11:35:10 +0200

    test run gha

--- src/action.mjs
+++ src/action.mjs
@@ -3,6 +3,7 @@
 import * as cmd from 'node:child_process'
 import * as path from 'node:path'
 import * as url from 'node:url'
+import * as fs from 'node:fs'
 
 /** @type {import('@actions/core')} */ let core
 /** @type {import('./pages.mjs').build} */ let build
@@ -15,7 +16,7 @@ try {
   await main()
 } catch (err) {
   console.error(err)
-  core.setFailed(err.stack || err.message)
+  core?.setFailed(err.stack || err.message)
   process.exit(1)
 }
 
@@ -24,6 +25,9 @@ try {
  */
 async function pre () {
   cmd.execSync('npm ci', { cwd: __dirname, stdio: 'inherit' })
+  for (const module of fs.readdirSync(path.join(__dirname, '..', 'modules'))) {
+    cmd.execSync('npm ci', { cwd: path.join(__dirname, '..', 'modules', module), stdio: 'inherit' })
+  }
   core = await import('@actions/core')
   build = (await import('./pages.mjs')).build
 }
