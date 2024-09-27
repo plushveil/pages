0597b8959d477dc411b729ad24c4c38d0ab66db5
plushveil <plushveil@gray.blue>
2024-09-27 12:18:38 +0200

    test run gha

---
+++ action/action.mjs
@@ -0,0 +1,22 @@
+import core from '@actions/core'
+import { build } from '../src/pages.mjs'
+
+try {
+  await main()
+} catch (err) {
+  console.error(err)
+  core.setFailed(err.stack || err.message)
+}
+
+/**
+ * Main action steps.
+ */
+async function main () {
+  const folder = core.getInput('folder')
+  const config = core.getInput('config')
+
+  console.log(`Building ${folder}${config ? ` with ${config}` : ''}`)
+  const output = await build(folder, config)
+
+  core.setOutput('folder', output)
+}
