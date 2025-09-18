c77eea2225afcf90d851eea646959592ab9f8816
Plushveil <plushveil@gray.blue>
2025-09-18 12:20:36 +0200

    chore: update package versions and dependencies
    - Bump @plushveil/pages version to 0.0.31
    - Update mime dependency to version 4.1.0
    - Upgrade eslint to version 9.35.0 and eslint-plugin-jsdoc to version 59.0.0
    - Update mocha to version 11.7.2
    - Bump vscode-extension version to 0.0.11
    - Upgrade typescript to version 5.9.2
    - Update @types/vscode to version 1.104.0
    - Update eslint in vscode-extension to version 9.35.0
    - Update package-lock.json with new dependency versions

--- npm-package/modules/html/addons/import-html.mjs
+++ npm-package/modules/html/addons/import-html.mjs
@@ -5,7 +5,7 @@ import * as url from 'node:url'
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const extensions = ['.html', '.htms', '.page']
+const extensions = ['.html', '.htms', '.page', '.svg']
 
 global.importAttributes = global.importAttributes || {}
 
