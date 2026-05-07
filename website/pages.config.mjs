46e64a700a2ebfbd7ccabdb4645c54738fc73af4
Plushveil <plushveil@gray.blue>
2026-05-07 11:44:13 +0200

    fix: update local server port from 3000 to 8080 in README.md and pages.config.mjs

--- website/pages.config.mjs
+++ website/pages.config.mjs
@@ -9,7 +9,7 @@
 export const baseURI = new URL(
   process.env.HOST
     ? (process.env.HOST.includes('://') ? process.env.HOST : `https://${process.env.HOST}`)
-    : 'http://localhost:3000'
+    : 'http://localhost:8080'
 )
 
 /**
